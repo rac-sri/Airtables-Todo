d8d56ed6a09deb63bee1fc055f74025f8e50e703
rachit2501 <rac.sri25@gmail.com>
2020-07-05 00:18:24 +0530

    user experince update next

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 - npm install
 - block run
 ```
+
+> Now you get an address. Create a block on airtable and enter te address. Your app will start running in the emulator.
--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,67 @@
-import {initializeBlock} from '@airtable/blocks/ui';
-import React from 'react';
+import {
+  initializeBlock,
+  useBase,
+  useRecords,
+  expandRecord,
+  TextButton,
+  TablePickerSynced,
+  useGlobalConfig,
+} from "@airtable/blocks/ui";
+import React, { useState } from "react";
 
-function HelloWorldBlock() {
-    // YOUR CODE GOES HERE
-    return <div>Hello world 🚀</div>;
+// If you want to get the cell values from other fields,
+// you can use record.getCellValue() or record.getCellValueAsString().
+
+function TodoBlock() {
+  const base = useBase();
+  const globalConfig = useGlobalConfig();
+  const tableId = globalConfig.get("selectedTableId");
+  const table = base.getTableByIdIfExists(tableId);
+  const records = useRecords(table);
+  //  const [tableId, setTableId] = useState(null)
+
+  const tasks = records
+    ? records.map((record) => <Task key={record.id} record={record} />)
+    : null;
+
+  return (
+    <div>
+      {/* <TablePicker
+        table={table}
+        onChange={(newTable) => {
+          //   setTableId(newTable.id);
+          globalConfig.setAsync("selectedTableId", newTable.id);
+        }}
+      /> */}
+      <TablePickerSynced globalConfigKey="selectedTableId" />
+      {tasks}
+    </div>
+  );
 }
 
-initializeBlock(() => <HelloWorldBlock />);
+function Task({ record }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        fontSize: 18,
+        padding: 12,
+        borderBottom: "1px solid #ddd",
+      }}
+    >
+      {record.name || "Unnamed record"}{" "}
+      <TextButton
+        icon="expand"
+        aria-label="Expand record"
+        variant="dark"
+        onClick={() => {
+          expandRecord(record);
+        }}
+      />
+    </div>
+  );
+}
+
+initializeBlock(() => <TodoBlock />);
